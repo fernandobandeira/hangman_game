1864d9efdad37fff8fdb660c30092c957efae403
Fernando Bandeira <fernando.bandeira94@gmail.com>
2020-08-27 09:34:35 -0300

    Add more tests

--- apps/hangman/test/game_test.exs
+++ apps/hangman/test/game_test.exs
@@ -9,6 +9,28 @@ defmodule GameTest do
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
+
+    tally = Game.tally(game)
+
+    assert tally.turns_left == game.turns_left
+    assert tally.game_state == :initializing
+    assert tally.used == MapSet.new()
+    assert Enum.count(tally.letters) > 0
+  end
+
+  test "new_game with word returns structure" do
+    game = Game.new_game("wibble")
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+
+    tally = Game.tally(game)
+
+    assert tally.turns_left == game.turns_left
+    assert tally.game_state == :initializing
+    assert tally.used == MapSet.new()
+    assert tally.letters == ["_", "_", "_", "_", "_", "_"]
   end
 
   test "state isn't changed for :won or :lost game" do
@@ -38,8 +60,17 @@ defmodule GameTest do
   test "a good guess is recognized" do
     game = Game.new_game("wibble")
     game = Game.make_move(game, "w")
+
     assert game.game_state == :good_guess
     assert game.turns_left == 7
+
+    tally = Game.tally(game)
+
+    assert tally.turns_left == game.turns_left
+    assert tally.game_state == :good_guess
+    assert tally.used == MapSet.new()
+      |> MapSet.put("w")
+    assert tally.letters == ["w", "_", "_", "_", "_", "_"]
   end
 
   test "a guessed word is a won game" do
@@ -63,22 +94,31 @@ defmodule GameTest do
   test "a bad guess is recognized" do
     game = Game.new_game("wibble")
     game = Game.make_move(game, "x")
+
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
+
+    tally = Game.tally(game)
+
+    assert tally.turns_left == game.turns_left
+    assert tally.game_state == :bad_guess
+    assert tally.used == MapSet.new()
+      |> MapSet.put("x")
+    assert tally.letters == ["_", "_", "_", "_", "_", "_"]
   end
 
   test "lost is recognized" do
     moves = [
       {"a", :bad_guess},
-      {"b", :bad_guess},
       {"c", :bad_guess},
       {"d", :bad_guess},
-      {"e", :bad_guess},
       {"f", :bad_guess},
-      {"g", :lost}
+      {"g", :bad_guess},
+      {"h", :bad_guess},
+      {"j", :lost}
     ]
 
-    game = Game.new_game("w")
+    game = Game.new_game("wibble")
 
     moves
     |> Enum.with_index()
@@ -86,6 +126,14 @@ defmodule GameTest do
       game = Game.make_move(game, guess)
       assert game.game_state == state
       assert game.turns_left == 7 - (index + 1)
+
+      tally = Game.tally(game)
+
+      assert tally.turns_left == game.turns_left
+      assert tally.game_state == state
+      assert tally.used == game.used
+      assert tally.letters == ["_", "_", "_", "_", "_", "_"]
+
       game
     end)
   end
